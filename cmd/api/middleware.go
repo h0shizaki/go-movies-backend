e31c51a34bff232f96fc0a52ed0b1992adf6134e
Kitsune-fx <kant.cmd@gmail.com>
2021-12-20 20:29:12 +0700

    [Auth]

--- cmd/api/middleware.go
+++ cmd/api/middleware.go
@@ -1,10 +1,78 @@
 package main
 
-import "net/http"
+import (
+	"errors"
+	"log"
+	"net/http"
+	"strconv"
+	"strings"
+	"time"
+
+	"github.com/pascaldekloe/jwt"
+)
 
 func (app *application) enableCORS(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Access-Control-Allow-Origin", "*")
+		w.Header().Set("Access-Control-Allow-Headers", "*")
+
+		next.ServeHTTP(w, r)
+	})
+}
+
+func (app *application) checkToken(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Add("Vary", "Authorization")
+
+		authHeader := r.Header.Get("Authorizaiton")
+
+		if authHeader == "" {
+			// set for anonymous user
+		}
+
+		headerParts := strings.Split(authHeader, " ")
+
+		if len(headerParts) != 2 {
+			app.errorJSON(w, errors.New("Invalid auth header"))
+			return
+		}
+
+		if headerParts[0] != "Bearer" {
+			app.errorJSON(w, errors.New("Unauthorized - no bearer"))
+			return
+		}
+
+		token := headerParts[1]
+		claims, err := jwt.HMACCheck([]byte(token), []byte(app.config.jwt.secret))
+
+		if err != nil {
+			app.errorJSON(w, errors.New("Unauthorized - failed hmac check"))
+			return
+		}
+
+		if !claims.Valid(time.Now()) {
+			app.errorJSON(w, errors.New("Unauthorized - token expired"))
+			return
+		}
+
+		if !claims.AcceptAudience("mydomain.com") {
+			app.errorJSON(w, errors.New("Unauthorized - invalid audience"))
+			return
+		}
+
+		if claims.Issuer != "mydomain.com" {
+			app.errorJSON(w, errors.New("Unauthorized - invalid issuer"))
+			return
+		}
+
+		userID, err := strconv.ParseInt(claims.Subject, 10, 64)
+
+		if err != nil {
+			app.errorJSON(w, errors.New("Unauthorized "))
+			return
+		}
+
+		log.Println("Valid User :", userID)
 
 		next.ServeHTTP(w, r)
 	})
