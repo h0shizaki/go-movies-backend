8ca94ce720023c6b490a34a8eb1ddb4a951288bc
Kitsune-fx <kant.cmd@gmail.com>
2021-12-30 11:28:40 +0700

    [🎄 Add README]

---
+++ README.md
@@ -0,0 +1,13 @@
+# GO Backend  
+**HAPPY NEW YEAR AND MERRY CHRISTMAS PROJECT**  
+Using:  
+- Go  
+- PostgresSQL  
+
+## How to run  
+1. Import database from Database/go_movies.sql  
+2. Setting database in main.go line 47
+3. Type ```Powershell
+    go run ./cmd/api/. 
+```    
+Server will run on port 4000  
--- cmd/api/main.go
+++ cmd/api/main.go
@@ -44,9 +44,12 @@ func main() {
 
 	flag.IntVar(&cfg.port, "port", 4000, "Server port listen on")
 	flag.StringVar(&cfg.env, "env", "development", "Application environment (development|production)")
-	flag.StringVar(&cfg.db.dsn, "dsn", "postgres://postgres:22194@localhost/go_movies?sslmode=disable", "Postgres connection string")
+	flag.StringVar(&cfg.db.dsn, "dsn", "postgres://postgres:[password]@[host]/go_movies?sslmode=disable", "Postgres connection string")
 	flag.StringVar(&cfg.jwt.secret, "jwt-secret", "2dce505d96a53c5768052ee90f3df2055657518dad489160df9913f66042e160", "secret")
+
 	flag.Parse()
+
+	// cfg.jwt.secret = os.Getenv("GO_MOVIES_JWT")
 
 	logger := log.New(os.Stdout, "", log.Ldate|log.Ltime)
 
